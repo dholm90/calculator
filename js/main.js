b0f261a241640323bfface7aa01c89e3a10299ef
dholm90 <devanholm@gmail.com>
2022-07-14 11:41:31 -0400

    Code refactoring

--- js/main.js
+++ js/main.js
@@ -4,5 +4,16 @@ const body = document.querySelector('body');
 const wrapper = document.querySelector('.wrapper');
 const container = document.querySelector('.container');
 const screen = document.querySelector('.screen');
-const buttons = document.querySelector('buttons');
+const buttons = document.querySelectorAll('button').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log(item.value);
+    });
+});
 
+buttons;
+
+// for (let i = 0; i < buttons; i++) {
+//     buttons[i].addEventListener('click', () => {
+//         console.log(buttons[i].value);
+//     });
+// }
