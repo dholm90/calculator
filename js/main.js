dbb1b94b147364fa0392bdfbc428969f1247548d
dholm90 <devanholm@gmail.com>
2022-07-14 11:18:39 -0400

    Query Selectors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// Query Selectors
+
+const body = document.querySelector('body');
+const wrapper = document.querySelector('.wrapper');
+const container = document.querySelector('.container');
+const screen = document.querySelector('.screen');
+const buttons = document.querySelector('buttons');
+
