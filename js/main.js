9c84f04fc29db16774a511c3b9a39c6c62b89532
dholm90 <devanholm@gmail.com>
2022-07-17 23:18:38 -0400

    Refactored Code

--- js/main.js
+++ js/main.js
@@ -1,169 +1,111 @@
-// Query Selectors
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
 
-const body = document.querySelector('body');
-const wrapper = document.querySelector('.wrapper');
-const container = document.querySelector('.container');
-const screen = document.querySelector('.screen');
-const buttons = document.querySelectorAll('.btn');
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
 
-// Operation Functions
-const add = function (a, b) {
-    return a + b;
-};
-const subtract = function (a, b) {
-    return a - b;
-};
-const multiply = function (a, b) {
-    return a * b;
-};
-const divide = function (a, b) {
-    return a / b;
-};
-const operate = function (a, b, op) {
-    let result;
-    switch (op) {
-        case "+":
-            result = add(a, b);
-            break;
-        case "-":
-            result = subtract(a, b);
-            break;
-        case "x":
-            result = multiply(a, b);
-            break;
-        case "/":
-            result = divide(a, b);
-            break;
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+}
 
+function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = "0."
+        calculator.waitingForSecondOperand = false;
+        return
+    }
+
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+
+    if (firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+        return firstOperand + secondOperand;
+    } else if (operator === '-') {
+        return firstOperand - secondOperand;
+    } else if (operator === '*') {
+        return firstOperand * secondOperand;
+    } else if (operator === '/') {
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+}
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+function updateDisplay() {
+    const display = document.querySelector('.screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.buttons');
+keys.addEventListener('click', event => {
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches('button')) {
+        return;
+    }
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
         default:
-            break;
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
-    return result;
-}
 
-let a;
-let b;
-let c;
-let op;
-let runningTotal = 0;
-let total;
-let display;
-let temp = [];
-
-// Button Event Listeners
-buttons.forEach(btn => {
-    btn.addEventListener('click', function handleClick(event) {
-        let input = btn.innerText;
-        //Switch statement for inputs
-        switch (input) {
-            case "1":
-                temp.push(input);
-                //updateDisplay();
-                break;
-            case "2":
-                temp.push(input);
-                break;
-            case "3":
-                temp.push(input);
-                break;
-            case "4":
-                temp.push(input);
-                break;
-            case "5":
-                temp.push(input);
-                break;
-            case "6":
-                temp.push(input);
-                break;
-            case "7":
-                temp.push(input);
-                break;
-            case "8":
-                temp.push(input);
-                break;
-            case "9":
-                temp.push(input);
-                break;
-            case "0":
-                temp.push(input);
-                break;
-            case "+":
-                if (!a) {
-                    a = parseInt(temp.join(""), 10);
-                } else {
-                    c = parseInt(temp.join(""), 10);
-                    runningTotal = operate(a, c, "+");
-                    a = runningTotal;
-                }
-                temp = [];
-                op = "+";
-                break;
-            case "-":
-                if (!a) {
-                    a = parseInt(temp.join(""), 10);
-                } else {
-                    c = parseInt(temp.join(""), 10);
-                    runningTotal = operate(a, c, "-");
-                }
-                //console.log(a);
-                temp = [];
-                op = "-";
-                break;
-            case "x":
-                if (!a) {
-                    a = parseInt(temp.join(""), 10);
-                } else {
-                    c = parseInt(temp.join(""), 10);
-                    runningTotal = operate(a, c, "x");
-                }
-                //console.log(a);
-                temp = [];
-                op = "x";
-                break;
-            case "/":
-                if (!a) {
-                    a = parseInt(temp.join(""), 10);
-                } else {
-                    c = parseInt(temp.join(""), 10);
-                    runningTotal = operate(a, c, "/");
-                }
-                //console.log(a);
-                temp = [];
-                op = "/";
-                break;
-            case "=":
-                b = parseInt(temp.join(""), 10);
-                total = operate(a, b, op);
-                op = "";
-                console.log(total);
-
-
-                break;
-            case "C":
-                a = 0;
-                b = 0;
-                c = 0;
-                op = "";
-                temp = [];
-                runningTotal = 0;
-                total = 0;
-                break;
-            default:
-                break;
-        }
-        updateDisplay();
-        //console.log(btn.innerText);
-    });
+    updateDisplay();
 });
-
-const updateDisplay = function () {
-    if (!total) {
-        display = parseInt(temp.join(""), 10);
-    } else if (total) {
-        display = total;
-        total = "";
-
-    }
-    screen.innerText = display;
-}
-// Input funtions
